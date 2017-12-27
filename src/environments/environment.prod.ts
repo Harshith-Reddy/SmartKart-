7a79f4b9dca6036a0e9aed3a9089782130b99e69
Harshith Reddy <govindhy@mail.uc.edu>
2017-12-27 03:25:57 -0500

    1st Phase
    Initialized Components
    Built NavBar
    Defined Routes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDLTUsrRzWPPm1Yl0DE97ijRqJEAv4Xukg',
+    authDomain: 'smartkart-ece29.firebaseapp.com',
+    databaseURL: 'https://smartkart-ece29.firebaseio.com',
+    projectId: 'smartkart-ece29',
+    storageBucket: 'smartkart-ece29.appspot.com',
+    messagingSenderId: '969693414819'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDLTUsrRzWPPm1Yl0DE97ijRqJEAv4Xukg',
+    authDomain: 'smartkart-ece29.firebaseapp.com',
+    databaseURL: 'https://smartkart-ece29.firebaseio.com',
+    projectId: 'smartkart-ece29',
+    storageBucket: 'smartkart-ece29.appspot.com',
+    messagingSenderId: '969693414819'
+  }
 };
