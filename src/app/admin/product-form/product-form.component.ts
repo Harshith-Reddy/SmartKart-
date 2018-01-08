0cce121145822fbb6852736a7c62340ee74467cb
Harshith Reddy <govindhy@mail.uc.edu>
2018-01-08 15:09:40 -0500

    working version(with search feature) before adding DATATABLE

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { CategoryService } from '../../category-service.service';
 import { ProductService } from '../../product.service';
 import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import 'rxjs/add/operator/take';
 
 @Component({
   selector: 'app-product-form',
@@ -10,18 +12,41 @@ import { Router } from '@angular/router';
 })
 export class ProductFormComponent implements OnInit {
   categories$;
+  product = {};
+  id;
   constructor(
     private router: Router,
+    private route: ActivatedRoute,
     categorySevice: CategoryService,
     public productService: ProductService
   ) {
     this.categories$ = categorySevice.getCategories();
+
+    this.id = this.route.snapshot.paramMap.get('id');
+    if (this.id) {
+      this.productService.getProduct(this.id).take(1).subscribe(p => this.product = p);
+    }
   }
 
   ngOnInit() {
   }
   save(product) {
-  this.productService.create(product);
-  this.router.navigate(['/admin/products']);
+
+    if (this.id) {
+      this.productService.update(this.id , product);
+    }
+    else {
+      this.productService.create(product);
+    }
+    this.router.navigate(['/admin/products']);
+
+  }
+
+  delete() {
+    if (confirm('Are you sure you want to delete this item?')) {
+      this.productService.delete(this.id);
+      this.router.navigate(['/admin/products']);
+    }
+
   }
 }
